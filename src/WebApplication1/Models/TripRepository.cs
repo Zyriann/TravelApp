01b44df74febce1b3f9cebf11957d68f97da9ef9
Zyrian Gantuangco <zslice0915@hotmail.com>
2016-05-04 22:59:21 -0400

    added TripRepository

---
+++ src/WebApplication1/Models/TripRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class TripRepository
+    {
+        private TripContext db;
+
+        public TripRepository(TripRepository repository)
+        {
+            db = repository;
+        }
+
+        public void GetAllTrips()
+        {
+            return IEnumerable<Trips>;
+        }
+
+    }
+}
